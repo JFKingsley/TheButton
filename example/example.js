10524db0dbbbc66492c1ae4dbd976b0a670aeb1d
Jonathan Kingsley <me@jfkingsley.co.uk>
2014-08-20 14:26:54 +0100

    Fixed issue

--- example/example.js
+++ example/example.js
@@ -1,4 +1,16 @@
-var requestRedis = require('../main.js');
+var request = require('../main.js');
 
+request('http://httpbin.org/get', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body); // Print the google web page.
+  }
+});
+request.post({ url: 'http://httpbin.org/post',
+               followAllRedirects: true,
+               form: { foo: 'bar' } }, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body); // Print the google web page.
+  }
+});
 
 console.log("Example Started");
--- src/request-redis.js
+++ src/request-redis.js
@@ -1,1 +1,40 @@
-var request = require('request');
+var request = require('request'),
+    allowedFuncs = ['get', 'post', 'put', 'patch'];
+
+var rqs = function () {
+    var $this   = this,
+        fn      = request,
+        args    = Array.prototype.slice.call(arguments);
+
+        return setupFunctionWithCache($this, fn, args);
+};
+
+Object.keys(request).filter(function (key) {
+    return request[key] instanceof Function;
+}).forEach(function (key) {
+    var fn = request[key];
+    rqs[key] = function () {
+        console.log(key);
+        //TODO figure out why this only fires once per function call
+        var $this   = this,
+            args    = Array.prototype.slice.call(arguments);
+
+        setupFunctionWithCache($this, fn, args);
+    };
+
+});
+
+function setupFunctionWithCache(thisInstance, origFunc, args) {
+    if (this.client) {
+
+    } else {
+        return request.apply(thisInstance, args);
+    }
+}
+
+rqs.setupRedisCache = function(client, options) {
+    this.options = !options ? {} : options;
+    this.client = client;
+};
+
+module.exports = rqs;

3de90f01a14b9f6ff6f82ff3976838e2bcab4214
Jonathan Kingsley <me@jfkingsley.co.uk>
2014-08-27 01:15:08 +0100

    Added basics for caching/settings

--- example/example.js
+++ example/example.js
@@ -1,14 +1,20 @@
 var request = require('../main.js');
 
 request('http://httpbin.org/get', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body); // Print the google web page.
+  if (!(!error && response.statusCode == 200 && body)) {
+    console.log("FAILED!"); // Print the google web page.
   }
 });
 
-request.setupRedisCache({});
+var redisConfig = {
+    host: "pub-redis-10914.us-east-1-3.3.ec2.garantiadata.com",
+    port: 10914,
+    password: "e3b0c44298fc1c1"
+};
 
-request.post({ url: 'http://httpbin.org/post',
+request.setupRedisCache(redisConfig);
+
+request.get({cacheResponse: true, url: 'http://httpbin.org/cache/60',
                form: { foo: 'bar' } }, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body); // Print the google web page.
--- src/request-redis.js
+++ src/request-redis.js
@@ -1,13 +1,15 @@
 var request      = require('request'),
     async        = require('async'),
     redis        = require('redis'),
+    crypto       = require('crypto'),
+    Base64       = {encode:function(e){var b=new Buffer(e);return b.toString('base64');},decode:function(e){var b =new Buffer(e, 'base64'); return b.toString();}},
     allowedFuncs = ['get', 'post', 'put', 'patch'];
 
 var rqs = setupValues();
 
 function setupValues() {
     return function() {
-        if(!this.calledOnce) {
+        if (!this.calledOnce) {
             this.calledOnce = true;
 
             async.filter(Object.keys(request), function (key, cb) {
@@ -20,6 +22,8 @@ function setupValues() {
                             args    = Array.prototype.slice.call(arguments);
 
                         if (this.client) {
+                            $this.client  = this.client;
+                            $this.options = this.options;
                             return setupFunctionWithCache($this, fn, args);
                         } else {
                             return fn.apply($this, args);
@@ -47,22 +51,82 @@ function setupValues() {
 }
 
 function setupFunctionWithCache(thisInstance, origFunc, args) {
+    if (args[0].cacheResponse && thisInstance.client) {
+        var callback    = args.pop(),
+            argsHash = args[0];
 
-    var callback = args.pop();
+        delete argsHash.cacheResponse;
+        delete argsHash.cacheExpiry;
 
-    args.push(function (err, res, body) {
-        callback(err, res, body);
-    });
+        argsHash = JSON.stringify(argsHash);
+        argsHash = crypto.createHash('sha1').update(argsHash).digest('hex');
 
-    origFunc.apply(thisInstance, args);
+        var redisKey = [argsHash];
+
+        if (thisInstance.options.prefix && thisInstance.options.prefix instanceof String) {
+            key.unshift(thisInstance.options.prefix);
+        }
+console.log(redisKey);
+        thisInstance.client.get(redisKey, function(err, reply) {
+            if (reply) {
+
+            } else {
+                args.push(function (err, res, body) {
+                    //Set all the header keys to lowercase, just to be sure
+                    var key, keys = Object.keys(res.headers),
+                        n         = keys.length;
+                    while (n--) {
+                      key = keys[n];
+                      res.headers[key.toLowerCase()] = res.headers[key];
+                    }
+
+                    //Checks if there are any caching params set and obeys them
+                    var canCache = true,
+                        expire   = args.cacheExpiry || 3600;
+
+                    if (!res.headers['cache-control'] || res.headers['cache-control'].split(',').indexOf('no-cache') > -1 || !res.headers.pragma || res.headers.pragma.toLowerCase() === 'no-cache' || res.headers['cache-control'].split(',').indexOf('private') > -1) {
+                        canCache = false;
+                    }
+
+                    if (!res.headers['cache-control'] || res.headers['cache-control'].split(',').indexOf('must-revalidate') > -1 || args.cacheResponse) {
+                        canCache = true;
+                    }
+
+                    if (canCache) {
+                        //Save body to cache
+                        console.log(body + redisKey);
+                        callback(err, res, body);
+                    } else {
+                        //Return the response with no caching
+                        return callback(err, res, body);
+                    }
+                });
+
+                //Fire function call
+                origFunc.apply(thisInstance, args);
+            }
+        });
+    } else {
+        origFunc.apply(thisInstance, args);
+    }
 }
 
 rqs.setupRedisCache = function(client, options) {
     this.options = !options ? {} : options;
-    this.client = client;
 
-    this.calledOnce = false;
-    rqs = setupValues();
+    if (client instanceof redis.RedisClient) {
+        this.client = client;
+    } else if (client instanceof Object){
+        this.client = redis.createClient(client);
+        if (client.password) {
+            this.client.auth(client.password);
+        }
+    }
+
+    if(this.client) {
+        this.calledOnce = false;
+        rqs = setupValues();
+    }
 };
 
 module.exports = rqs;

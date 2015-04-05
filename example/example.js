f2e1a91bf1646374e75a148fd99df8711a0358b4
Jonathan Kingsley <me@jfkingsley.co.uk>
2015-04-05 01:06:27 +0100

    minor changes

--- example/example.js
+++ example/example.js
@@ -5,6 +5,11 @@ api.getNewToken(function(tokenData) {
 
     api.connect(tokenData.token, tokenData.epoch);
 
+    api.on('tick', function(data) {
+      console.log('tick');
+      console.log(data);
+    })
+
     api.on('buttonReset', function(data) {
       console.log('reset');
       console.log(data);
